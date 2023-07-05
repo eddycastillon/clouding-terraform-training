147259d9682727fc9d5fc83beb5d31c85b2fb57a
Eddy Castillon <eddy.castillon@gmail.com>
2023-07-05 07:33:40 -0500

    manejo de state1

--- session1/local-file.tf
+++ session1/local-file.tf
@@ -1,4 +1,23 @@
+
+resource "aws_instance" "angles11" {
+  instance_type = "t2.micro"
+  key_name      = "test"
+  ami = "ami-06b09bfacae1453cb"
+}
+
+
 resource "local_file" "foo" {
   content  = "foo!"
   filename = "${path.module}/foo.bar"
+}
+
+resource "local_file" "angeles_del_mar" {
+  content  = "foo!"
+  filename = "${path.module}/angeles_del_mar"
+}
+
+
+resource "local_file" "Jilver" {
+  content  = "hhhh"
+  filename = "${path.module}/Jilver"
 }
